e9c75653bafbe1f6d29658ad88042c2ea6690af3
Chaewon Yoon <104494969+jamie7dev@users.noreply.github.com>
2023-05-29 18:12:26 -0700

    Time: 77 ms (13.28%), Space: 44.9 MB (38.38%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -4,18 +4,11 @@
  */
 var createCounter = function(init) {
     let counter = init;
-    function increment() {
-        return ++counter;
-    }
-    function decrement() {
-        return --counter;
-    }
-    function reset() {
-        counter = init;
-        return counter;
-    }
+    const increment = () => ++counter;
+    const decrement = () => --counter;
+    const reset = () => counter = init;
     
-    return {increment: increment, decrement: decrement, reset: reset}
+    return {increment, decrement, reset}
 };
 
 /**
